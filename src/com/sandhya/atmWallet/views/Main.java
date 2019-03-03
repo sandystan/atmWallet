2ae6aa1a2ae5749540563a0d757c44fd336b2b82
Sandhya <sandhyareddyn1998@gmail.com>
2019-03-03 13:07:27 +0530

    Main

--- src/com/sandhya/atmWallet/views/Main.java
+++ src/com/sandhya/atmWallet/views/Main.java
@@ -8,56 +8,53 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
-import com.sandhya.atmWallet.views.oldClasses.Login;
-import com.sandhya.atmWallet.views.oldClasses.Sign_up;
 import com.sandhya.atmWallet.views.oldClasses.User;
 
-public class Main extends JFrame implements ActionListener{
-	 private static Main main;
-	    JLabel l1,l2,l3;
-	    JTextField t1;
-	    JPasswordField t2;
-	    JButton b;
-	    JPanel p;
-	    public Main(){
-	        l1 = new JLabel("User Name : ");
-	        l2 = new JLabel("PIN : ");
-	        l3 = new JLabel("");
-	        t1 = new JTextField(10);
-	        t2 = new JPasswordField(8);
-	        b = new JButton("Sign_Up");
-	        p = new JPanel();
-	        p.add(l1);
-	        p.add(t1);
-	        p.add(l2);
-	        p.add(t2);
-	        p.add(l3);
-	        p.add(b);
-	        b.addActionListener(this);
-	        p.setBackground(new Color(34,49,63));
-	        l1.setForeground(new Color(228,251,254));
-	        l2.setForeground(new Color(228,251,254));
-	        l3.setForeground(new Color(228,251,254));
-	        b.setBackground(new Color(228,251,254));
-	        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-	        getContentPane().add(p);
-	    }
-	    public static void main(String[] args) {
-	        main = new Main();
-	        main.setVisible(true);
-	        main.setSize(500,500);
-	        main.setBackground(new Color(34,49,63));
-	    }
-	    @Override
-	    public void actionPerformed(ActionEvent e) {
-	        User ur = new User();
+public class Main extends JFrame implements ActionListener {
+	/**
+	* 
+	*/
+	private static final long serialVersionUID = 1L;
+	private static Main main;
+	JLabel appName, welcomeMsg, developedBy;
+	JButton btnSignUp;
+	JPanel mainPannel;
+
+	public Main() {
+		appName = new JLabel("POCKECT ATM WALLET");
+		welcomeMsg = new JLabel("We trust in the way you trust us");
+		btnSignUp = new JButton("Sign_Up");
+		mainPannel = new JPanel();
+
+		mainPannel.add(appName);
+		mainPannel.add(welcomeMsg);
+		mainPannel.add(btnSignUp);
+		
+		btnSignUp.addActionListener(this);
+		mainPannel.setBackground(new Color(34, 49, 63));
+		appName.setForeground(new Color(228, 251, 254));
+		welcomeMsg.setForeground(new Color(228, 251, 254));
+		//developedBy.setForeground(new Color(228, 251, 254));
+		btnSignUp.setBackground(new Color(228, 251, 254));
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		getContentPane().add(mainPannel);
+	}
+
+	public static void main(String[] args) {
+		main = new Main();
+		main.setVisible(true);
+		main.setSize(500, 500);
+		main.setBackground(new Color(34, 49, 63));
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		User ur = new User();
 		/*
 		 * ur.sign_up(t1.getText(),Integer.parseInt(t2.getText())); m.setVisible(false);
 		 * Login lg = new Login(ur,this); lg.setVisible(true); lg.setSize(500,500);
 		 */
-	    }
+	}
 }
